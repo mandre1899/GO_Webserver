2dff3c10c38df4ac2b7f7d07c8f47e1bfbd015c8
KijijiKid <mateo-andre@gmx.de>
2026-05-04 16:31:49 +0200

    feat: exercise 3

--- internal/server/server.go
+++ internal/server/server.go
@@ -13,6 +13,11 @@ func (s *WebServer) CreateServer() {
 		Addr: ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "text/plain; charset=uft-8")
+		w.Write([]byte("OK"))
+	})
 }
 
